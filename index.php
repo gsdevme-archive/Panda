10c8945e913f552ee8f4e12fbc5fe14293895d1c
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 00:59:55 +0100

    Added true,true to the spl_autoloader_register, also added a throw exception

--- index.php
+++ index.php
@@ -2,13 +2,19 @@
 
     require_once 'Core/config.php';
 
-    spl_autoload_register(function($name)
-    {
-        $class = str_replace('\\', '/', $name);
+    try {
 
-        if (is_readable($filename)) {
+        spl_autoload_register(function($name){
+            $class = str_replace('\\', '/', $name);
+
+            if (is_readable($filename)) {
+                require_once $filename;
+            }
             
-        }
-    });
-
+            throw new Core\Exceptions\AutoloaderException('Panda Autoloader could not find ' . $name . ' Class. Check the Spelling of the Class and the Filename');
+        }, true, true);
+    } catch (Exception $e) {
+        //temp
+        die($e->getMessage());
+    }
     
