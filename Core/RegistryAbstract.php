b69c7297afa60cd51939d72b313cd41a7e64c114
John White <john@phpremium.com>
2011-07-07 00:19:16 +0100

    Added an import method, still not tested

--- Core/RegistryAbstract.php
+++ Core/RegistryAbstract.php
@@ -7,6 +7,7 @@ abstract class RegistryAbstract
     
     private static $_instance;
     
+    private $_store;
     /**
      * Get an instance of a registry
      * @return RegistryAbstract
@@ -17,5 +18,16 @@ abstract class RegistryAbstract
             static::$_instance = new static;
         }
         return static::$_instance;
+    }
+    
+    /**
+     * Import an associative array or stdClass
+     * @param type $data 
+     */
+    public function import($data)
+    {
+        foreach($data as $k=>$v){
+            $this->$k = $v;
+        }
     }
 }
