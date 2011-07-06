f30538caefc53456ec9826ed54af16cbc6ff3215
gav <gav@gav-laptop.(none)>
2011-07-07 00:39:11 +0100

    Changed the index.php to be an anonymous function

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 
     require_once 'Core/config.php';
 
-    function pandaAutoload($name)
+    spl_autoload_register(function($name)
     {
         $class = str_replace('\\', '/', $name);
 
         if (is_readable($filename)) {
             
         }
-    }
-
-    spl_autoload_register('PandaAutoload');
+    });
 
     
