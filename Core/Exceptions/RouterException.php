d209e0355a31849a4f4426571afc16cea14d3b40
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 14:28:20 +0100

    Added a Router Exception

---
+++ Core/Exceptions/RouterException.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Core\Exceptions;
+
+    class RouterException extends ExceptionAbstract
+    {
+
+        public function __construct($message, $code=null, \Exception $previous=null)
+        {
+            parent::__construct($message, $code, $previous);
+        }
+
+    }
