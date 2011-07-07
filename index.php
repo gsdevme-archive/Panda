db91676f123428de8d38a1c9a0d96307e799cf6d
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 12:59:18 +0100

    Added Panda call with Import of the Global Config.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-
-    require_once 'Core/Functions.php';
     require_once 'Core/Config.php';
+    require_once 'Core/Functions.php';    
 
     try {
         spl_autoload_register(function($class) {
@@ -15,8 +14,11 @@
                 throw new \Core\Exceptions\AutoloaderException('Panda Autoloader could not find ' . $name . ' Class. Check the Spelling of the Class and the Filename');
             }, true, true);
 
+        Core\Panda::getInstance()->import($config);
         Core\Router::route(new \Core\Request());
     } catch (Exception $e) {
         //temp
         die($e->getMessage());
     }
+
+    echo (memory_get_usage() - Core\Panda::getInstance()->memory) / 1024 . ' kb';
