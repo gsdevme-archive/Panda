172c3894864fcf934e37e6f6ee2850985a8af8ae
John White <john@phpremium.com>
2011-07-07 20:24:36 +0100

    mend

---
+++ Core/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Core;
+
+abstract class Controller
+{
+    private $_registry;
+    
+    public function index();
+    
+    public function __construct()
+    {
+        $this->_registry = Registry::getInstance();
+    }
+    final protected function model($name)
+    {
+        if(!isValue($this->_registry->$name)){
+            
+        }
+        return $this->_registry->$name;
+    }
+    final protected function library($name)
+    {
+        
+    }
+    final protected function serviceLayer($name){
+        
+    }
+}
