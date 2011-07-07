870c9bee325550aa3bc30bcb0e3911f886045504
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 10:14:08 +0100

    Request working for routing although not virtualhosts

--- Core/Request.php
+++ Core/Request.php
@@ -9,32 +9,35 @@
 
         /**
          * Parse and Find the Controller/Method/Args
+         * @param string $request 
          */
-        public function __construct()
+        public function __construct($request=null)
         {
-            /*
-             * Command Line Detection
-             * php -f index.php controller/method/args virtualhost
-             */
-            if ((defined('PHP_SAPI')) && (PHP_SAPI === 'cli')) {
-                if (isValue($_SERVER['argv'][1])) {
-                    $request = $_SERVER['argv'][1];
+            if ($request === null) {
+                /*
+                 * Command Line Detection
+                 * php -f index.php controller/method/args virtualhost
+                 */
+                if ((defined('PHP_SAPI')) && (PHP_SAPI === 'cli')) {
+                    if (isValue($_SERVER['argv'][1])) {
+                        $request = $_SERVER['argv'][1];
 
-                    if (isValue($_SERVER['argv'][2])) {
-                        $virtualhost = $_SERVER['argv'][2];
+                        if (isValue($_SERVER['argv'][2])) {
+                            $virtualhost = $_SERVER['argv'][2];
+                        }
                     }
                 }
-            }
 
-            /*
-             * Http Detection
-             * http://virtualhost/index.php/controller/method/args
-             */
-            if (isValue($_SERVER['REQUEST_URI'])) {
-                $_SERVER['REQUEST_URI'] = substr(str_replace($_SERVER['SCRIPT_NAME'], null, $_SERVER['REQUEST_URI']), 1);
-                
-                if(isValue($_SERVER['REQUEST_URI'])){
-                    $request = $_SERVER['REQUEST_URI'];
+                /*
+                 * Http Detection
+                 * http://virtualhost/index.php/controller/method/args
+                 */
+                if (isValue($_SERVER['REQUEST_URI'])) {
+                    $_SERVER['REQUEST_URI'] = substr(str_replace($_SERVER['SCRIPT_NAME'], null, $_SERVER['REQUEST_URI']), 1);
+
+                    if (isValue($_SERVER['REQUEST_URI'])) {
+                        $request = $_SERVER['REQUEST_URI'];
+                    }
                 }
             }
 
@@ -42,11 +45,19 @@
             $this->_virtualHost = ifsetor($virtualhost, 'Index');
         }
 
+        /**
+         * Common sense
+         * @return string 
+         */
         public function getRequest()
         {
             return $this->_request;
         }
 
+        /**
+         * Common sense
+         * @return string 
+         */
         public function getVirtualHost()
         {
             return $this->_virtualHost;
