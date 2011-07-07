5556624f03b4adeb2de44b74ee12e125a7b1eb38
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 14:28:35 +0100

    Added detection for App, some planning with comments

--- Core/Router.php
+++ Core/Router.php
@@ -6,12 +6,20 @@
     {
 
         /**
-         * 
          * @param Request $request 
          */
         public static function route(Request $request)
         {
-            echo '<pre>' . print_r($request, 1) . '</pre>';
+            // Check App
+            $app = (is_readable(Panda::getInstance()->sys . $request->getApp()) . '/Config.php') ? $request->getApp() : Panda::getInstance()->defaultApp . '/Config.php';
+
+            //Load Config from App
+            
+            echo '<pre>App:' . print_r($app, 1) . '</pre>';
+
+            // Check Controller exists 
+            // Check Method exists
+            // Check any args and does the method take them
         }
 
     }
