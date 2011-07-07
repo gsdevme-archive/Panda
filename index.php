4fdf49191a6cfff35aa518b6aba9c67db7b456b0
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-07 14:28:13 +0100

    Added Sys into the anonymous function

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
+
     require_once 'Core/Config.php';
-    require_once 'Core/Functions.php';    
+    require_once 'Core/Functions.php';
+    $sys = realpath(dirname(__FILE__)) . '/';
 
     try {
-        spl_autoload_register(function($class) {
-                $filename = str_replace('\\', '/', $class) . '.php';
+        spl_autoload_register(function($class) use ($sys) {
+                $filename = $sys . str_replace('\\', '/', $class) . '.php';
 
                 if (is_readable($filename)) {
                     require_once $filename;
                     return;
                 }
 
-                throw new \Core\Exceptions\AutoloaderException('Panda Autoloader could not find ' . $name . ' Class. Check the Spelling of the Class and the Filename');
+                throw new \Core\Exceptions\AutoloaderException('Panda Autoloader could not find ' . $class . ' Class. Check the Spelling of the Class and the Filename');
             }, true, true);
 
-        Core\Panda::getInstance()->import($config);
+        $panda = Core\Panda::getInstance()->import($config);
+        $panda->sys = $sys;
+
         Core\Router::route(new \Core\Request());
     } catch (Exception $e) {
         //temp
