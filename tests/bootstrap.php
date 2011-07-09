6da3ca2bba5cc002d961157db946506958dcf8d3
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-09 18:59:05 +0100

    Added PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,30 @@
 <?php
+    $root = realpath(dirname(__FILE__)) . '/../';
+    require_once $root . 'Core/Config.php';
+    require_once $root . 'Core/Exceptions/ExceptionAbstract.php';
+    require_once $root . 'Core/Exceptions/AutoloaderException.php';
+    require_once $root . 'Core/Functions.php';
+    require_once $root . 'Core/RegistryAbstract.php';
+    require_once $root . 'Core/Panda.php';
+    require_once $root . 'Core/Registry.php';
 
-    spl_autoload_register(function($name)
-            {
-                $file = './' . str_replace('\\', '/', $name) . '.php';
-                if (is_readable($file)) {
-                    require_once $file;
-                }
-            });
-
+    $panda = Core\Panda::getInstance()->import($config);
+    $panda->root = $root;
     
+	spl_autoload_register(function($class) use ($panda) {
+			$file = str_replace('\\', '/', $class) . '.php';
+
+			$rootFile = $panda->root . $file;
+			$appRootFile = $panda->appRoot . $file;
+
+			if (is_readable($rootFile)) {
+				require_once $rootFile;
+				return;
+			}
+
+			if (is_readable($appRootFile)) {
+				require_once $appRootFile;
+				return;
+			}
+
+		}, true, true);
