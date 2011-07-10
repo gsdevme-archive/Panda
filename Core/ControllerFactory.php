5261d7849f564c0341727eba5375eed6fedb6ea0
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-10 22:43:52 +0100

    Added static method for re-routing

--- Core/ControllerFactory.php
+++ Core/ControllerFactory.php
@@ -4,9 +4,12 @@
 
 use \ReflectionClass as ReflectionClass;
 use \ReflectionMethod as ReflectionMethod;
+use \ReflectionException as ReflectionException;
+use Core\Exceptions\RouterException as RouterException;
 
     class ControllerFactory
     {
+
         /**
          * Invokes the controller 
          * @param ReflectionClass $class
@@ -14,28 +17,60 @@ use \ReflectionMethod as ReflectionMethod;
          * @param array $args
          * @return type 
          */
-        public function __construct(ReflectionClass $class, ReflectionMethod $method, array $args=null){
-            if($args === null){
+        public function __construct(ReflectionClass $class, ReflectionMethod $method, array $args=null)
+        {
+            if ($args === null) {
                 return $method->invoke($class->newInstance(), null);
             }
-            
+
             return $method->invokeArgs($class->newInstance(), $this->_urldecode($args));
         }
-        
+
+        /**
+         * Invokes a controller, this should be used to re-route 
+         * @param string $controller
+         * @param string $method
+         * @param array $args 
+         */
+        public static function route($controller, $method='index', array $args=null)
+        {
+
+            try {
+                $class = new ReflectionClass('Controllers\\' . ucfirst($controller));
+                $method = new ReflectionMethod($class->name, $method);
+
+                if ($class->isInstantiable()) {
+                    if ((Panda::getInstance()->mode == 'HTTP') && ($class->getParentClass()->name != 'Controllers\Controller')) {
+                        throw new RouterException('This controller is for CLI use only, Controller: ' . ucfirst($controller), 404);
+                    }
+
+                    if (($method->isPublic()) && (!$method->isConstructor())) {
+                        return new self($class, $method, $args);
+                    }
+                }
+            } catch (ReflectionException $e) {
+                
+            }
+
+            throw new RouterException('Panda failed the find the controller or method, with the name ' . ucfirst($controller), 404, ifsetor($e, null));
+        }
+
         /**
          * Perhaps a URL decode on each argument
          * @param array $args
          * @return array 
          */
-        private function _urldecode(array $args){
+        private function _urldecode(array $args)
+        {
             $count = count($args);
-            
-            for($i=0;$i<$count;++$i){
+
+            for ($i = 0; $i < $count; ++$i) {
                 $args[$i] = urldecode($args[$i]);
             }
-            
+
             return $args;
         }
+
     }
 
 ?>
