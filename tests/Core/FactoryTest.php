6e86bc330076cba2678a2fa5de1ca14cdb435543
John White <john@phpremium.com>
2011-07-10 16:31:34 +0100

    More tests

--- tests/Core/FactoryTest.php
+++ tests/Core/FactoryTest.php
@@ -18,16 +18,35 @@ use PHPUnit_Framework_TestCase as PHPUnit;
             $this->_panda->appRoot = $this->_panda->root . 'Index/';
             $this->_registry = Registry::getInstance();
         }
+        
+        public function testShared()
+        {
+            return;
+            //all this will fail with the current code, well the next method would fail, this one would be fine but this stuff is what needs changing for them both to work.
+            try {
+                $model = Factory::model('users',1);
+                $this->assertTrue($model instanceof \Core\Model, 'Model not an instance of \Core\Model');
 
+                $library = Factory::library('twitter',1);
+                $this->assertTrue(is_object($library), 'Library is not an object');
+
+                $sl = Factory::serviceLayer('users',1);
+                $this->assertTrue($sl instanceof \Core\ServiceLayer, 'Not an instance of service layer');
+                
+            } catch (Exception $e) {
+                $this->fail($e->getMessage());
+            }
+        }
+        
         public function testExistant()
         {
             try {
                 $model = Factory::model('users');
                 $this->assertTrue($model instanceof \Core\Model, 'Model not an instance of \Core\Model');
-                
+
                 $library = Factory::library('twitter');
                 $this->assertTrue(is_object($library), 'Library is not an object');
-                
+
                 $sl = Factory::serviceLayer('users');
                 $this->assertTrue($sl instanceof \Core\ServiceLayer, 'Not an instance of service layer');
                 
@@ -35,10 +54,9 @@ use PHPUnit_Framework_TestCase as PHPUnit;
                 $this->fail($e->getMessage());
             }
         }
-        public function testShared()
-        {
-            
-        }
+
+
+
         public function testNonExistentModel()
         {
             try {
