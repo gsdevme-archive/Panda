9ffbd0d663ba582798de8169dd70543373ceccdb
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-10 23:35:18 +0100

    Added Nicer Errors

--- Core/ErrorReport.php
+++ Core/ErrorReport.php
@@ -19,33 +19,40 @@ use \Exception as Exception;
 
         private function _HTMLOutput($e)
         {
-            $HTMLOutput = '<h2>Panda Error</h2>';
-            $HTMLOutput .= '<h3>' . $e->getMessage() . '</h3>';
-            return $HTMLOutput;
+            if (is_object($e)) {
+                $HTMLOutput = '<div style="border:1px solid red;padding:5px 10px;margin:5px 0;"><h3>Panda Error</h3>';
+                $HTMLOutput .= '<h4 style="font-weight:400;">' . $e->getMessage() . '</h4>';                
+                $HTMLOutput .= '<h4 style="font-weight:400;">' . $e->getPrevious()->getMessage() . '</h4></div>';
+
+                $HTMLOutput .= $this->_getTrace($e);
+                return $HTMLOutput;
+            }
         }
-        
-        private function _CLIOutput($e){
+
+        private function _CLIOutput($e)
+        {
             $CLIOutput = "\n#--- Panda Error -------------------------#\n";
             $CLIOutput .= "- " . $e->getMessage() . " \n";
             $CLIOutput .= "#-----------------------------------------#\n";
-            
+
             return $CLIOutput;
         }
 
-        private function _getTrace($e, $callback)
+        private function _getTrace($e)
         {
+            if (($trace = $e->getTrace())) {
+                $traceOutput = '<table width="100%" border="1" cellpadding="5px"><tr><td>File</td><td>Line</td></tr>';
 
-            /* if (($trace = $e->getTrace())) {
-              echo '<table width="100%" border="1" cellpadding="5px"><tr><td>File</td><td>Line</td></tr>';
+                foreach ($trace as $error) {
+                    if (isset($error['file'], $error['line'])) {
+                        $traceOutput .= '<tr><td>' . $error['file'] . '</td><td>' . $error['line'] . '</td></tr>';
+                    }
+                }
 
-              foreach ($trace as $error) {
-              if (isset($error['file'], $error['line'])) {
-              echo '<tr><td>' . $error['file'] . '</td><td>' . $error['line'] . '</td></tr>';
-              }
-              }
+                $traceOutput .= '</table>';
 
-              echo '</table>';
-              } */
+                return $traceOutput;
+            }
         }
 
         public function getOutput()
