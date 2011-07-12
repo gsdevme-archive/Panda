737931f4df466da56b4d7ca9fcee8a8e9832feef
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-12 18:19:20 +0100

    Debug Code / Tests

--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -7,24 +7,7 @@
 
         public function index()
         {
-            $this->view('dummy', array(
-                'message' => 'hello world',
-            ))->render();
-
-            return;
-            //1 flag means shared, if the param is missing it gets the app model
-            // $this->library('Twitter', 1)->doSomething();
-
-            $this->serviceLayer('Users')->doSomething();
-
-            echo '<pre>' . print_r(\Core\Registry::getInstance(), 1) . '</pre>';
-        }
-
-        public function something()
-        {
-            echo '<pre>' . print_r(func_get_args(), 1) . '</pre>';
-
-            echo '<pre>' . print_r(\Core\Panda::getInstance(), 1) . '</pre>';
+            $this->view('index', null, false)->render();
         }
 
     }
---
+++ Index/Views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>		
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title>Awesome Website</title>
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+    </head>
+
+    <body>
+        <div id="container">
+            <p>Hello</p>
+        </div>
+    </body>
+</html>
