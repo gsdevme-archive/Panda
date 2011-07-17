fec5e5444881b8500a798f1887bd41358888f944
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-17 21:56:29 +0100

    Added new View loading method

--- Core/View.php
+++ Core/View.php
@@ -2,92 +2,41 @@
 
     namespace Core;
 
-use Core\Exceptions\ViewException as Exception;
-
     class View
     {
 
-        private $_panda;
-        private $_registry;
-        private $_file;
         private $_args;
 
-        public function __construct()
+        public function __construct($file, array $args=null)
         {
-            $this->_panda = Panda::getInstance();
-            $this->_registry = Registry::getInstance();
-        }
-
-        /**
-         * Load a view
-         * @param string $name name of the view
-         * @param array $args array of arguments
-         * @param bool $shared is this a shared view?
-         * @return type 
-         */
-        public function load($name, array $args = null, $shared = false)
-        {
-            if ($shared === true) {
-                $file = $this->_panda->root . '/Shared/Views/' . $name . '.php';
-            } else {
-                $file = $this->_panda->appRoot . 'Views/' . $name . '.php';
-            }
-
-            if (is_readable($file)) {
-                $this->_file = $file;
+            if ($args !== null) {
                 $this->_args = $args;
-                return $this;
-            }
-            throw new Exception('Failed to load ' . $name. ' - '.$file);
-        }
-        
-        /**
-         * Add arguments to the view
-         * @param type $x
-         * @param type $z 
-         */
-        public function args($x, $z = false)
-        {
-            if (is_array($x)) {
-                array_merge($this->_args, $z);
-            } elseif (isset($x, $z)) {
-                $this->_args[$x] = $z;
-            }
-        }
-        
-        /**
-         * Render a view
-         * @return View 
-         */
-        public function render()
-        {
-            if (is_array($this->_args)) {
                 extract($this->_args);
             }
-            require $this->_file;
-            return $this;
+
+            require $file;
         }
-        
+
         /**
          * Load an element
-         * @param type $name
-         * @param type $shared 
+         * @param string $name
+         * @param bool $shared 
          */
         public function element($name, $shared = false)
         {
             if ($shared === true) {
-                $file = $this->_panda->root . '/Shared/Elements/' . $name . '.php';
+                $file = Panda::getInstance()->root . '/Shared/Elements/' . $name . '.php';
             } else {
-                $file = $this->_panda->appRoot . 'Elements/' . $name . '.php';
+                $file = Panda::getInstance()->appRoot . 'Elements/' . $name . '.php';
             }
-            if(is_readable($file)){
-                //give the element access to this views args
-                if(is_array($this->_args)){
+
+            if (is_readable($file)) {
+                if (is_array($this->_args)) {                    
                     extract($this->_args);
                 }
                 require $file;
-            }else{
-                throw new Exception('Element not found - '.$file);
+            } else {
+                throw new Exception('Element not found - ' . $file);
             }
         }
 
---
+++ Core/ViewFactory.php
@@ -0,0 +1,119 @@
+<?php
+
+    namespace Core;
+
+use Core\Exceptions\ViewException as ViewException;
+
+    class ViewFactory
+    {
+
+        private static $_instance;
+        private $_views;
+        private $_currentView;
+
+        private function __construct()
+        {
+            $this->_views = array();
+        }
+
+        /**
+         * Returns instance
+         * @return type 
+         */
+        public static function getInstance()
+        {
+            if (!self::$_instance instanceof self) {
+                self::$_instance = new self;
+            }
+
+            return self::$_instance;
+        }
+
+        /**
+         * Add a view to the ADT Queue
+         * @param string $name
+         * @param array $data
+         * @param bool $shared 
+         */
+        public function addView($name, array $args=null, $shared=false)
+        {
+            if ($shared === true) {
+                $file = Panda::getInstance()->root . '/Shared/Views/' . $name . '.php';
+            } else {
+                $file = Panda::getInstance()->appRoot . 'Views/' . $name . '.php';
+            }
+
+            // Checksum
+            $this->_currentView = sprintf('%u', crc32($file));
+
+            if (!isset($this->_views[$this->_currentView])) {                     
+                if (is_readable($file)) {
+                    $this->_views[$this->_currentView] = ( object ) array('file' => $file, 'args' => $args, 'name' => $name);
+                    return self::$_instance;
+                }
+
+                throw new ViewException('Failed to load view, could not find ' . $view->name, 404, null);
+            }
+            
+            return self::$_instance;
+        }
+
+        /**
+         * Render all views 
+         * @return type 
+         */
+        public function render()
+        {
+            if (!empty($this->_views)) {
+                foreach ($this->_views as $view) {
+                    new View($view->file, $view->args);
+                }
+
+                return true;
+            }
+
+            throw new ViewException('No views where found, make sure you use $this->view() before $this->render()');
+        }
+
+        /**
+         * Add values to argument array
+         * @param string $property
+         * @param type $value 
+         */
+        private function _args($property, $value)
+        {            
+            if (isset($this->_views[$this->_currentView])) {
+                $this->_views[$this->_currentView]->args[$property] = $value;
+            }
+        }
+
+        /**
+         * Add values to argument array
+         * @param string $property
+         * @param type $value 
+         */
+        private function _argsArray(array $args)
+        {
+            if (isset($this->_views[$this->_currentView])) {
+                $this->_views[$this->_currentView]->args = array_merge($this->_views[$this->_currentView]->args, $args);
+            }
+        }
+
+        /**
+         * To overload _args() and argsArray()
+         * @param type $method
+         * @param type $args
+         * @return type 
+         */
+        public function __call($method, $args)
+        {            
+            if (($method === 'args') && (isset($args[0]))) {
+                if ((!is_array($args[0])) && (isset($args[1]))) {
+                    return $this->_args($args[0], $args[1]);
+                }
+
+                return $this->_argsArray($args[0]);
+            }
+        }
+
+    }
