c9aa9683b18af65b31241fe33d308d80b79c55a8
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-17 21:57:40 +0100

    Debug Code

--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -7,7 +7,15 @@
 
         public function index()
         {
-            $this->view('index', null, false)->render();
+            $data = array();
+            
+            $data['list'] = range(1,10);            
+            $this->view('index', $data);
+
+            $this->view('index')->args('test', 'hello does this work ?');
+            $this->view('index')->args(array('foobar' => 'yes this also works'));
+            
+            $this->render();
         }
 
     }
