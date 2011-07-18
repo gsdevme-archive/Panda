b4e3b700484f50b53b0792cc8f4e92cfbf006526
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-18 13:23:39 +0100

    New ControllerFactoryTest

--- tests/Core/ControllerFactoryTest.php
+++ tests/Core/ControllerFactoryTest.php
@@ -5,16 +5,72 @@
     class ControllerFactoryTest extends PHPUnit
     {
 
-        public function __construct()
+        public function setup()
         {
-            // Removes Unit testing info
-            $_SERVER['argv'] = array();
+            $config = new stdClass;
 
-            // Adds server params needed
-            $_SERVER['SCRIPT_NAME'] = 'index.php';
+            $config->defaultApp = 'Index';
+            $config->debug = true;
+            $config->defaultController = 'Index';
+            $config->defaultMethod = 'Index';
+            $config->mode = 'HTTP';
+            $config->appRoot = \Core\Panda::getInstance()->root . 'Index/';
+
+            \Core\Panda::getInstance()->import($config);
         }
 
-        public function test(){
-            
+        public function testInvoke()
+        {
+            try {
+                $class = new ReflectionClass('\\Controllers\Index');
+                $method = new ReflectionMethod($class->name, 'index');
+                
+                $return = new \Core\ControllerFactory($class, $method, null);
+            } catch (\Exception $e) {
+                $this->fail($e);
+            }
         }
+
+        public function testInvokeWithReoute()
+        {
+            try {                
+                $return = \Core\ControllerFactory::route('Index', 'Index');
+            } catch (\Exception $e) {
+                $this->fail($e);
+            }
+        }
+
+        public function testInvokeWithWrongParams()
+        {
+            try {
+                $return = \Core\ControllerFactory::route('BillBob', 'Index');
+                
+                $this->fail('Failed, as the ControllerFactory didnt error');
+            } catch (\Exception $e) {
+                
+            }
+        }
+
+        public function testInvokeWithWrongParamsAgain()
+        {
+            try {
+                $return = \Core\ControllerFactory::route('Dummy', 'Index');
+                
+                $this->fail('Failed, as the ControllerFactory didnt error');
+            } catch (\Exception $e) {
+                
+            }
+        }
+
+        public function testInvokeWithNonInstantiable()
+        {
+            try {
+                $return = \Core\ControllerFactory::route('Controller', 'Index');
+                
+                $this->fail('Failed, as the ControllerFactory didnt error');
+            } catch (\Exception $e) {
+                
+            }
+        }
+
     }
