b702a8e049fb5aedd254a50756cec4f7edf6c436
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-19 13:08:51 +0100

    Added all the methods from controller

--- Core/ServiceLayer.php
+++ Core/ServiceLayer.php
@@ -2,29 +2,9 @@
 
     namespace Core;
 
-    abstract class ServiceLayer
+    abstract class ServiceLayer extends Controller
     {
 
-        final protected function model($name, $shared = false)
-        {
-            return Factory::model($name, $shared);
-        }
-
-        /**
-         * Load a library
-         * @param string $name name of the library
-         * @param bool $shared is this a shared library?
-         * @return object
-         */
-        final protected function library($name, $shared = false)
-        {
-            return Factory::library($name, $shared);
-        }
-
-        final protected function view($name, array $args = null, $shared = false)
-        {
-            return Factory::view($name, $args, $shared);
-        }
 
     }
 
