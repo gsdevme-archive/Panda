7440caef6fff90c4660d984f4a8ea24239fd3722
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-19 13:25:09 +0100

    Added share class

---
+++ Shared/Libraries/Share.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace Libraries;
+
+    /**
+     * Dummy class shipped to show an example, you can either edit this or remove it.
+     */
+    class Share
+    {
+
+        public function doSomething()
+        {
+            return 'Shared Library Shared called with the method doSomething';
+        }
+
+    }
+
+    
