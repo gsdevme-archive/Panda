894773db6fe58d483ece19335242c0a293a006a9
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-19 23:37:34 +0100

    Added abstract methods

--- Core/Model.php
+++ Core/Model.php
@@ -4,7 +4,35 @@
 
     abstract class Model
     {
-        
+
+        private $_registry;
+
+        public function __construct()
+        {
+            $this->_registry = Registry::getInstance();
+        }
+
+        /**
+         *
+         * @param type $name
+         * @return type 
+         */
+        public function __get($name)
+        {
+            return $this->modules($name);
+        }
+
+        public function modules($name)
+        {
+            if(($module = $this->_registry->modules($name))){
+                return $module;
+            }
+            
+            $class = '\Etc\Modules\\' . $name;
+            
+            return $this->_registry->modules($name, new $class);
+        }
+
     }
 
     
