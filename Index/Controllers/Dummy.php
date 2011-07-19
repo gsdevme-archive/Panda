47960e08169c4f468e8100e59321030f5f7e0a6d
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-19 13:08:39 +0100

    Dummy Code

--- Index/Controllers/Dummy.php
+++ Index/Controllers/Dummy.php
@@ -2,18 +2,15 @@
     namespace Controllers;
 
     /**
-     * This is a dummy class just to show how you secure your 
-     * controllers to CLI only, although Controller will work aswell
-     * but can be access over HTTP
+     * This is a dummy class just to show how you secure your controllers to CLI only, 
+     * although Controller will work with CLI if you wish
      */
     class Dummy extends CLIController
     {
 
         public function index()
         {
-            echo '<pre>' . print_r(func_get_args(), 1) . '</pre>';
-
-            echo '<pre>' . print_r(\Core\Panda::getInstance(), 1) . '</pre>';            
+            $this->view('dummy');    
         }
 
     }
