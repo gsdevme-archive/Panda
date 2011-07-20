f7e6d24ff80cb917b02eba008f839e9be14581d5
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-20 22:38:19 +0100

    Added module loading

--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,11 @@
 
     namespace Core;
 
+use \Exception as Exception;
+use \Core\Exceptions\ModuleException as ModuleException;
+use \ReflectionClass as ReflectionClass;
+use \ReflectionMethod as ReflectionMethod;
+
     abstract class Model
     {
 
@@ -24,13 +29,27 @@
 
         public function modules($name)
         {
-            if(($module = $this->_registry->modules($name))){
+            if (($module = $this->_registry->modules($name))) {
                 return $module;
             }
-            
-            $class = '\Etc\Modules\\' . $name;
-            
-            return $this->_registry->modules($name, new $class);
+
+            try {
+                $class = '\Etc\Modules\\' . ucfirst($name);
+                $class = new ReflectionClass($class);
+
+                if ($class->isInstantiable()) {
+                    return $this->_registry->modules($name, $class->newInstance());
+                }
+
+                if ($class->hasMethod('getInstance')) {
+                    $class = $class->name;
+                    return $this->_registry->modules($name, $class::getInstance());
+                }
+            } catch (Exception $e) {
+                
+            }
+
+            throw new ModuleException('Panda failed to load module ' . ucfirst($name), 500, ifsetor($e, null));
         }
 
     }
