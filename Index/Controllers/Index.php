849a8552d3c5fe2218c7210ec82aa831be6e7d16
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-22 17:27:09 +0100

    Removed Debug code

--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -7,16 +7,9 @@
 
         public function index()
         {            
-            $this->view('index')->args('serviceLayer', $this->serviceLayer('Dummy')->doSomething());
-            
-            $this->view('index')->args('library', $this->library('Dummy')->doSomething());
             
             $this->view('index')->args('model', $this->model('Dummy')->doSomething());
-            
-            $this->view('index')->args('libraryShared', $this->library('Share', true)->doSomething());
-            
-            $this->view('index')->args('modelShared', $this->model('Share', true)->doSomething());
-            
+
             $this->render();
         }
 
