ca92cd53d1366b5e3034b0ca86a3c803c4844a78
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-22 17:27:18 +0100

    Added PDO DB Class

---
+++ Etc/Modules/PDO.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace Etc\Modules;
+
+    class PDO implements iDb
+    {
+
+        private static $_instance;
+
+        private function __construct()
+        {
+            
+        }
+
+        public static function getInstance()
+        {
+            if (!self::$_instance instanceof self) {
+                self::$_instance = new self;
+            }
+
+            return self::$_instance;
+        }
+
+        public function query()
+        {
+            
+        }
+
+    }
+
+    
