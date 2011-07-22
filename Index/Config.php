eea833541ca6912f8bf6349e4355da58c18af327
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-22 16:47:05 +0100

    Added Default Values

--- Index/Config.php
+++ Index/Config.php
@@ -4,3 +4,7 @@
     $config->debug = true;
     $config->defaultController = 'Index';
     $config->defaultMethod = 'Index';
+    
+    $config->dbClass = 'PDO';
+    
+    $config->sessionClass = 'Session';
