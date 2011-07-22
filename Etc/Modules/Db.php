4b3c6f1acb1f4416a86dc418a5175d85d778c4e8
Gavin Staniforth <gavshouse90@gmail.com>
2011-07-22 17:27:26 +0100

    Added DB Model

--- Etc/Modules/Db.php
+++ Etc/Modules/Db.php
@@ -2,14 +2,25 @@
 
     namespace Etc\Modules;
 
-    class Db implements iDb
+    use \Core\Panda as Panda;
+    use Core\Exceptions\ModuleException as ModuleException;
+    
+    class Db
     {
 
         private static $_instance;
+        private $_db;
 
         private function __construct()
-        {
+        {            
+            if(isset(Panda::getInstance()->dbClass)){
+                $dbClass = '\Etc\Modules\\' . Panda::getInstance()->dbClass;                
+                $this->_db = $dbClass::getInstance();
+                
+                return;
+            }
             
+            throw new ModuleException('You must define which class to use for the Database within your Config.php for your application, $config->dbClass = \'PDO\';', 500, null);
         }
 
         public static function getInstance()
@@ -18,11 +29,11 @@
                 self::$_instance = new self;
             }
 
-            return self::$_instance;
+            return self::$_instance->getDb();
         }
         
-        public function query(){
-            
+        public function getDb(){
+            return $this->_db;
         }
 
     }
