2e3c6085a2546a4141d61f03925c7c53e96d67a2
Gavin Staniforth <gavshouse90@gmail.com>
2011-08-03 16:30:42 +0100

    Added test code

--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -7,8 +7,11 @@
 
         public function index()
         {            
-            
             $this->view('index')->args('model', $this->model('Dummy')->doSomething());
+            
+            $this->view('index')->args('library', $this->library('Dummy')->doSomething());
+            
+            $this->view('index')->args('dbTest', $this->model('Dummy')->testDbConnection());
 
             $this->render();
         }
