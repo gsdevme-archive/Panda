812aa1b371da552b1303f8cdb6ead1457e913f28
Gavin Staniforth <gavshouse90@gmail.com>
2011-08-03 16:30:36 +0100

    Added default config

--- Index/Config.php
+++ Index/Config.php
@@ -5,10 +5,10 @@
     $config->defaultController = 'Index';
     $config->defaultMethod = 'Index';
     
-    /*$config->dbClass = 'PDO';
+    $config->dbClass = 'PDO';
     $config->dbHost = 'mysql:host=127.0.0.1;dbname=test;port=3306';
     $config->dbUser = 'root';
-    $config->dbPass = 'root';
-    $config->dbInitCmd = 'SET NAMES \'UTF8\'';*/
+    $config->dbPass = 'rot';
+    $config->dbInitCmd = 'SET NAMES \'UTF8\'';
     
     $config->sessionClass = 'Session';
