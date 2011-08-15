bc0d14b822cae153065e774e56c8d13dc640a74f
Gavin Staniforth <gavshouse90@gmail.com>
2011-08-15 22:59:36 +0100

    Added Session Configuration

--- Index/Config.php
+++ Index/Config.php
@@ -5,10 +5,11 @@
     $config->defaultController = 'Index';
     $config->defaultMethod = 'Index';
     
-    $config->dbClass = 'PDO';
     $config->dbHost = 'mysql:host=127.0.0.1;dbname=test;port=3306';
     $config->dbUser = 'root';
-    $config->dbPass = 'rot';
+    $config->dbPass = 'root';
     $config->dbInitCmd = 'SET NAMES \'UTF8\'';
     
-    $config->sessionClass = 'Session';
+    /* To enable sessions to be stored within the Application folder 
+    uncomment this line and create a folder /AppName/Sessions */
+    //$config->sessionClass = 'SessionHandler';
