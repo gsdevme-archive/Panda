75202938ebaf224cbfc7c14b4a003ab90c39f23d
Gav <gavshouse90@gmail.com>
2011-10-15 02:08:03 +0100

    Added validation usage example

--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -35,6 +35,14 @@
         {
             return $this->route('Index', 'Index');
         }
+        
+        public function user($username, $password){
+            if($this->validation('User')->username($username)){
+                die('Yes valid user');
+            }
+            
+            die('Nope, not a valid user');
+        }        
 
     }
 
