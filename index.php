17d7eac059e08a7ed4468bcfddf0cffbd070fc76
Gav <gavshouse90@gmail.com>
2011-11-17 20:53:45 +0000

    Added unset() on $config and $root to help with memory

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
     $panda->root = $root;
     $panda->thirdParty = $root . 'Etc/ThirdParty/';
 
+    //Remove to help with memory
+    unset($config);
+    unset($root);
+
     set_error_handler(function($errno, $errstr, $errfile, $errline ) {
             throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
         });
@@ -37,8 +41,14 @@
 
                 throw new \Core\Exceptions\AutoloaderException('Panda Autoloader could not find ' . $class . ' Class. Check the Spelling of the Class and the Filename');
             }, true, true);
+            
         new Core\Router(new \Core\Request());
     } catch (Exception $e) {
+        if (ob_get_status() !== false) {
+            // Remove anything from the buffer
+            ob_end_clean();
+        }
+
         $errorReport = new \Core\ErrorReport($e);
         echo $errorReport->getOutput();
         exit;
